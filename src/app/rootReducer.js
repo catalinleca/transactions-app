8d052eb0162fa5acd76202a57c3417a909942be7
Catalin Leca <leca.catalin1995@gmail.com>
2021-11-21 13:24:54 +0200

    setting up store

---
+++ src/app/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+// import transactionReducer from "views/transactions/transactionsSlice";
+
+const rootReducer = combineReducers({
+  // transactions: transactionReducer
+});
+
+export default rootReducer;
